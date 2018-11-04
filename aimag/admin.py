a941dc3dd6dc2e63ada2c65e628e78258278711d
Lkhagva <nlkhagva@Lkhagva-Ochirs-MacBook-Pro.local>
2018-11-04 15:35:30 +0800

    aimag, sum model adeed

--- aimag/admin.py
+++ aimag/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Aimag, Sum
+
+admin.site.register(Aimag)
+admin.site.register(Sum)
--- aimag/models.py
+++ aimag/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
 # Create your models here.
+class Aimag(models.Model):
+    name        = models.CharField(max_length=30)
+    sum_count   = models.IntegerField(default=0)
+    hun_am      = models.IntegerField(default=0)
+    talbai      = models.IntegerField(default=0)
+    nyagtarshil = models.FloatField(default=0)
+    aimag_tuv   = models.CharField(max_length=50)
+    hursh_aimag = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.name    
+
+class Sum(models.Model):
+    name        = models.CharField(max_length=50)
+    aimag       = models.ForeignKey(Aimag, on_delete=models.CASCADE)
+    bag_count   = models.IntegerField(default=0)
+    hun_am      = models.IntegerField(default=0)
+    talbai      = models.IntegerField(default=0)
+    nyagtarshil = models.FloatField(default=0)
+    hursh_sum   = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.name
+
